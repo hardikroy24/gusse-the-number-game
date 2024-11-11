8d5e6a2f14643e3fa9e62a6c738cd155710e8685
HARDIK RAY <hardikroyee34@gmail.com>
2024-11-11 22:05:57 +0530

    Create gusse.java
    play in your editor

---
+++ gusse.java
@@ -0,0 +1,36 @@
+ //gusse-the-number-game
+ import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
+        int numberOfTries = 0;
+        int guess;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to Guess the Number Game!");
+        System.out.println("I have chosen a number between 1 and 100. Try to guess it!");
+
+        // Game loop
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the number in " + numberOfTries + " tries.");
+            }
+        }
+    
+
+    }
+}
